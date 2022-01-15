68413a35811f8d73606b2f77f3d05f209447ad52
SteveMiller7 <info@millercreativedesign.com>
2022-01-15 10:41:40 +0000

    Add welcome function which displays welcom info, instructions and input for customers name.

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('decorating_estimator')
+
+costs = SHEET.worksheet('costs')
+
+data = costs.get_all_values()
+
+
+def welcome():
+    """
+    Welcome information and input for relevant customer name for the job being estimated.
+    """
+    print("Welcome to the Room Decorating Estimator.")
+    print("Please input the required information when prompted.")
+    
+    cust_name = input('Enter customer name here:\n')
+
+welcome()
+
+    
